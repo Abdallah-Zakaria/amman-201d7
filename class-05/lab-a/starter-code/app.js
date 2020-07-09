e192fb9684953ab35c14f9fb84eae9db49bacb6d
Your Name In Single QuotesAbdallah-Zakariagit config --global core.editor code <abdallah.zakaria1996@icloud.com>
2020-07-09 14:33:39 +0300

    problem2

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -8,9 +8,9 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-function sum(a, b) { //eslint-disable-line
+// function sum(a, b) { //eslint-disable-line
 
-}
+// }
 
 // Here is the test for sum(); uncomment it to run it
 // testSum(4, 7);
@@ -26,12 +26,18 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
+var a= 0 ;
+var b = 0;
 function multiply(a, b) { //eslint-disable-line
-
+    var multiply= a * b;
+    var returnArray =[ multiply , 'The product of '+a+' and '+ b+ ' is 45.' ];
+    return returnArray;
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+console.log(multiply(5,9));
+multiply(5,9);
+testMultiply();
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 

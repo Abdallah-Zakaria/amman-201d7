8c455b7ba2afe873323238c0fa28341d7cdaefcb
Your Name In Single QuotesAbdallah-Zakariagit config --global core.editor code <abdallah.zakaria1996@icloud.com>
2020-07-09 17:02:16 +0300

    problem4

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -59,19 +59,19 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-    var totalSum =sum(a,b)[0];
-    var totalSum = sum(totalSum , c)[0];
+    var totalSum = sum(a, b)[0];
+    var totalSum = sum(totalSum, c)[0];
 
-    var totalMultiply = multiply(a,b)[0];
+    var totalMultiply = multiply(a, b)[0];
     var totalMultiply = multiply(totalMultiply, c)[0];
-    
-    var returnArray =[totalSum , totalMultiply ,  a+' and '+b+' and '+c+' sum to '+totalSum+'.','The product of '+a+' and '+b+' and '+c+' is '+totalMultiply+'.'];
+
+    var returnArray = [totalSum, totalMultiply, a + ' and ' + b + ' and ' + c + ' sum to ' + totalSum + '.', 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + totalMultiply + '.'];
     return returnArray;
 
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-testSumAndMultiply(4,7,5);
+testSumAndMultiply(4, 7, 5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -87,16 +87,20 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 var testArray = [2, 3, 4]; //eslint-disable-line
-
+var sumList = 0;
+var returnArray = [];
 function sumArray(sumArr) { //eslint-disable-line
 
-    var returnArray = [, testArray + " was passed in as an array of numbers, and 9 is their sum."]
-
+    var totalSum1 = sum(sumArr[0], sumArr[1])[0];
+    var totalSum = sum(totalSum1, sumArr[2])[0];
+    var msg = testArray + ' was passed in as an array of numbers, and ' + totalSum + ' is their sum.'
+    // returnArray = [totalSum, testArray + ' was passed in as an array of numbers, and ' + sumList + ' is their sum.']
+    return [totalSum, msg];
 }
-
+console.log(sumArray(testArray));
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
